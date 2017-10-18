6e71a09bb43e0cb06cf23458dbd48b4f5d1157de
RobinManhas <28775512+RobinManhas@users.noreply.github.com>
2017-10-18 15:43:06 -0400

    remove .o files

--- VPC3/Debug/src/LVPredictor.d
+++
@@ -1,6 +0,0 @@
-src/LVPredictor.o: ../src/LVPredictor.cpp ../src/LVPredictor.h \
-  ../src/Predictor.h
-
-../src/LVPredictor.h:
-
-../src/Predictor.h:
--- VPC3/Debug/src/main.d
+++
@@ -1,5 +0,0 @@
-src/main.o: ../src/main.cpp ../src/VPC3.h ../src/Predictor.h
-
-../src/VPC3.h:
-
-../src/Predictor.h:
